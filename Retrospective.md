64884ffbdb882b5ac2db1a824c23819d0a5a1adf
briancole989 <briancole989@u.boisestate.edu>
2024-11-04 14:39:00 -0700

    Submitting Project 4 for CS208

--- Retrospective.md
+++ Retrospective.md
@@ -1,16 +1,18 @@
 # Retrospective
 
-- name: Jane Doe
-- email: janedoe@u.boisestate.edu
+- name: Brian Cole
+- email: briancole989@u.boisestate.edu
 
 ## Experience
 
-TODO
+This project ( although very fun, ) was a comaplete nightmare at times. I struggled to get the 'tasks.length' variable in 'index' to stop crashing on me for at least an hour. I probably couldve started on a workaround earlier, but I had a couple OTHER problems going on and wasn't sure what the best problem to focus on was. Eventually I found the 'redirect' method that served as a 'saving grace' to my overheating mental engine. I'm certain this project wasn't as nearly as I made it out to be, but for a good portion of the project I spent my time rereading through the zybook reading to find something I felt I must have been missing. DESPITE these issues, I feel this was defenitely a good experiene overall as learning not only how to develop both front and backend projects is invalueble to me as a a student looking to enter the jobfield, but the integration of back TO frontend is a side of things I always wondered. 
 
 ## Known issues or Bugs
 
-TODO
+N/A
+As far as I am AWARE, there are no bugs here anymore
 
 ## Sources used
 
-TODO
+Zybook reading - 
+https://learn.zybooks.com/zybook/BOISESTATECS208PutnamFall2024/chapter/9/section/3
--- src/server.js
+++ src/server.js
@@ -18,24 +18,55 @@ app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
+let local = { tasks: [] }
+
 app.get('/', function (req, res) {
     //TODO You will need to do a SQL select here
     //TODO You will need to update the code below!
+    local = { tasks: [] }
+
+    db.each('SELECT id, task FROM todo', function (err, row) {
+        if (err) {
+        console.log(err)
+        } else {
+        local.tasks.push({ id: row.id, task: row.task })
+        }
+    }, function (err, numrows) {
+        if (!err) {
+        res.render('index', local)
+        } else {
+        console.log(err)
+        res.render('index')
+    }
+  }) 
+   
     console.log('GET called')
-    res.render('index')
 
 })
+
+app.get('/delete', function (req, res) {
+    res.render('index',local)
+}) 
 
 app.post('/', function (req, res) {
     console.log('adding todo item')
     //TODO You will need to to do a SQL Insert here
+    const stmt = db.prepare('INSERT INTO todo (task) VALUES (?)')
+    stmt.run(req.body.todo)
+    stmt.finalize()
+
+    res.redirect("back")
 
 })
 
 app.post('/delete', function (req, res) {
     console.log('deleting todo item')
     //TODO you will need to delete here
+    const stmt = db.prepare('DELETE FROM todo where id = (?)')
+    stmt.run(req.body.id)
+    stmt.finalize()
 
+    res.redirect("back")
 })
 
 // Start the web server
